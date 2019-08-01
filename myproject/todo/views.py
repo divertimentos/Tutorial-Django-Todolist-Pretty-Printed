e17c7dfb7d324809c540f5a8929d7897cec1abf4
guiemi <guiemi.teixeira@gmail.com>
2019-08-01 17:59:29 -0300

    Finaliza o setup inicial do projeto em Django

---
+++ myproject/todo/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import Todo
+
+def index(request):
+    todo_list = Todo.objects.order_by('id')
+    
+    context = {'todo_list': todo_list}
+    
+    return render(request, 'todo/index.html', context)
+
+
